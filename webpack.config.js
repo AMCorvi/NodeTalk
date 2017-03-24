3459b3dddc5c3f15d4fc54256ea238f6fe272918
AMCorvi <amcorvi@icloud.com>
2017-03-24 16:43:45 -0400

    - Webpack Config

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,28 @@
+var webpack = require("webpack");
+var path = require('path');
 
-
-module.export = {
-    
-    }
+module.exports = {
+	entry: {
+		app: "./public/app/App.js"
+	},
+	output: {
+		filename:"build/bundle.js",
+        sourceMapFilename: "build/bundle.map"
+	},
+    devtool: '#source-map',	
+	// plugins: [
+ //    	new webpack.optimize.UglifyJsPlugin({minimize: true}),
+	// ],	
+	module: {
+		loaders: [
+			{
+				test: /\.jsx?$/,
+				exclude: /(node_modules|bower_components)/,
+				loader: 'babel',
+				query:{
+					presets:['react', 'es2015']
+				}
+			}
+		]
+	}
+}
